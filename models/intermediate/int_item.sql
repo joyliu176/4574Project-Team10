056249c7b29c2e3f485910505126e4cc769c457b
Yaping Liu <yl5569@columbia.edu>
2024-04-18 21:49:41 +0000

    Modify int_item table

--- models/intermediate/int_item.sql
+++ models/intermediate/int_item.sql
@@ -1,10 +1,15 @@
 SELECT 
     ITEM_NAME,
-    ARRAY_AGG(DISTINCT PRICE_PER_UNIT) AS price_array
+    ARRAY_AGG(DISTINCT PRICE_PER_UNIT) AS price_array,
+    SUM(ADD_TO_CART_QUANTITY) AS total_added_to_cart,
+    SUM(REMOVE_FROM_CART_QUANTITY) AS total_removed_from_cart,
+    COUNT(*) AS total_views,
 FROM (
     SELECT 
         ITEM_NAME,
-        PRICE_PER_UNIT
+        PRICE_PER_UNIT,
+        ADD_TO_CART_QUANTITY,
+        REMOVE_FROM_CART_QUANTITY
     FROM {{ ref('base_web_schema__item_views') }}
     ORDER BY ITEM_NAME, PRICE_PER_UNIT
 )
