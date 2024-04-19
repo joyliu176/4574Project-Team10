85870b2e6392b2032a73ac566bef822d5a79f8d5
Cecilia Lin <tl3240@columbia.edu>
2024-04-19 00:30:40 +0000

    Modify intermediate layer

--- models/intermediate/int_client.sql
+++ models/intermediate/int_client.sql
@@ -1,21 +1,29 @@
-SELECT 
-    s.client_id, 
-    s.ip, 
-    o.shipping_address, 
-    o.state, 
-    o.client_name, 
-    o.phone
-FROM {{ ref('base_web_schema__sessions') }} s
-INNER JOIN {{ ref('base_web_schema__orders') }} o
-    ON s.session_id = o.session_id
-INNER JOIN (
-    SELECT 
-        s_inner.client_id, 
-        MAX(o_inner.order_date_ts) AS max_order_date_ts
-    FROM {{ ref('base_web_schema__sessions') }} s_inner
-    INNER JOIN {{ ref('base_web_schema__orders') }} o_inner
-        ON s_inner.session_id = o_inner.session_id
-    GROUP BY s_inner.client_id
-) AS latest
-    ON s.client_id = latest.client_id 
-    AND o.order_date_ts = latest.max_order_date_ts
+WITH OrderSessionInfo AS (
+  SELECT
+    s.client_id,
+    s.ip,
+    s.os,
+    o.client_name,
+    o.phone,
+    o.payment_method,
+    o.payment_info,
+    o.shipping_address,
+    o.state,
+    o.order_date_ts,
+    ROW_NUMBER() OVER (PARTITION BY s.client_id ORDER BY o.order_date_ts DESC) AS rn
+  FROM {{ ref('base_web_schema__orders') }} o
+  JOIN {{ ref('base_web_schema__sessions') }} s
+    ON o.session_id = s.session_id
+)
+SELECT
+  client_id,
+  ip,
+  os,
+  client_name,
+  phone,
+  payment_method,
+  payment_info,
+  shipping_address,
+  state
+FROM OrderSessionInfo
+WHERE rn = 1
--- models/intermediate/int_session.sql
+++ models/intermediate/int_session.sql
@@ -18,28 +18,23 @@ CleanedPageViews AS (
            ROW_NUMBER() OVER (PARTITION BY SESSION_ID ORDER BY VIEW_TIME_TS ASC) AS rn
     FROM {{ ref('base_web_schema__page_views') }}
 )
-
 SELECT 
-    s.SESSION_ID,
-    s.CLIENT_ID,
-    s.IP,
-    s.OS,
-    s.SESSION_TIME_TS,
-    COUNT(DISTINCT p.PAGE_NAME) AS num_pages_visited,
-    ARRAY_AGG(DISTINCT p.PAGE_NAME) AS pages_visited,
-    SUM(i.ADD_TO_CART_QUANTITY) AS total_items_added,
-    SUM(i.REMOVE_FROM_CART_QUANTITY) AS total_items_removed,
-    total_items_added - total_items_removed AS total_items,
-    COUNT(DISTINCT o.ORDER_DATE_TS) AS num_orders,
-    ARRAY_AGG(DISTINCT o.PAYMENT_METHOD) AS payment_methods_used
+    cs.SESSION_ID,
+    cs.CLIENT_ID,
+    co.ORDER_ID AS order_id,
+    cs.IP,
+    cs.OS,
+    cs.SESSION_TIME_TS,
+    cp.PAGE_NAME AS page_visited,
+    ci.ITEM_NAME AS item_viewed,
+    ci.ADD_TO_CART_QUANTITY AS items_added,
+    ci.REMOVE_FROM_CART_QUANTITY AS items_removed,
+    co.PAYMENT_METHOD AS payment_method_used,
+    (CASE WHEN co.ORDER_ID IS NOT NULL THEN 1 ELSE 0 END) AS order_placed
 FROM 
-    CleanedSessions s
-LEFT JOIN CleanedPageViews p ON s.SESSION_ID = p.SESSION_ID AND p.rn = 1
-LEFT JOIN CleanedItemViews i ON s.SESSION_ID = i.SESSION_ID AND i.rn = 1
-LEFT JOIN CleanedOrders o ON s.SESSION_ID = o.SESSION_ID AND o.rn = 1
+    CleanedSessions cs
+LEFT JOIN CleanedPageViews cp ON cs.SESSION_ID = cp.SESSION_ID AND cp.rn = 1
+LEFT JOIN CleanedItemViews ci ON cs.SESSION_ID = ci.SESSION_ID AND ci.rn = 1
+LEFT JOIN CleanedOrders co ON cs.SESSION_ID = co.SESSION_ID AND co.rn = 1
 WHERE 
-    s.rn = 1
-GROUP BY 
-    s.SESSION_ID, s.CLIENT_ID, s.IP, s.OS, s.SESSION_TIME_TS
-HAVING 
-    COUNT(DISTINCT p.PAGE_NAME) IS NOT NULL
+    cs.rn = 1
