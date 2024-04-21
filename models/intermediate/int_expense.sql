a9e9a395cade5caf240c1f214a1061672a821f42
Cecilia Lin <tl3240@columbia.edu>
2024-04-21 03:13:02 +0000

    Modify int_expense table

--- models/intermediate/int_expense.sql
+++ models/intermediate/int_expense.sql
@@ -1,10 +1,17 @@
-SELECT 
+SELECT
+    ROW_NUMBER() OVER (ORDER BY DATE, EXPENSE_TYPE) AS row_num,
     EXPENSE_TYPE,
-    SUM(EXPENSE_AMOUNT) AS TOTAL_AMOUNT,
+    TOTAL_AMOUNT,
     DATE
-FROM 
-    {{ ref('base_google_drive__expenses') }}
-GROUP BY 
-    DATE, EXPENSE_TYPE
+FROM (
+    SELECT 
+        EXPENSE_TYPE,
+        SUM(EXPENSE_AMOUNT) AS TOTAL_AMOUNT,
+        DATE
+    FROM 
+        {{ ref('base_google_drive__expenses') }}
+    GROUP BY 
+        DATE, EXPENSE_TYPE
+) AS subquery
 ORDER BY 
     DATE, EXPENSE_TYPE
